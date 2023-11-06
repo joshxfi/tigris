7d49d18e5722c898ef91db5110c6e6b5f3b6fbba
Josh Daniel <joshxfi.dev@gmail.com>
2023-11-06 15:52:10 +0800

    chore: add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          100: "#FF8207",
+          200: "#FF682C",
+        },
+        secondary: {
+          100: "#A3A3A3",
+          200: "#212121",
+          300: "#1C1C1C",
+          400: "#111111",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
