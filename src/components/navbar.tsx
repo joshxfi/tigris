8ab38b959577288f12467e59904cd0c8012a208c
Josh Daniel <joshxfi.dev@gmail.com>
2023-11-08 11:15:18 +0800

    feat: add hero section

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export function Navbar() {
   return (
-    <nav className="flex justify-between items-center py-12">
+    <nav className="flex justify-between items-center py-12 text-sm">
       <h1 className="font-bold text-xl">tigris</h1>
 
-      <div className="flex space-x-24 text-secondary-100">
-        <ul className="lg:flex items-center space-x-24 hidden">
+      <div className="flex text-secondary-100">
+        <ul className="lg:flex items-center space-x-20 hidden">
           <li>Home</li>
           <li>Council</li>
           <li>Contact</li>
@@ -15,7 +15,7 @@ export function Navbar() {
 
         <Link
           href="/merch"
-          className="bg-secondary-200 px-6 py-3 rounded-md text-sm md:text-base"
+          className="bg-secondary-200 px-6 py-3 rounded-md"
         >
           Browse Merch
         </Link>
