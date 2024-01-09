108bc4d14726f5e039338943664aaf84924e54ea
Josh Daniel <joshxfi.dev@gmail.com>
2024-01-09 20:33:27 +0800

    feat: implement gsap

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,27 @@
+"use client";
+
+import gsap from "gsap";
+import { useRef } from "react";
 import Image from "next/image";
+import { useGSAP } from "@gsap/react";
 import arrow from "@/assets/arrow.svg";
 
 export function Header() {
+  const container = useRef<HTMLDivElement>(null);
+
+  useGSAP(
+    () => {
+      gsap.from("#engineering", { x: -800, duration: 1.5, ease: "power2.out" });
+      gsap.from("#technology", { x: 800, duration: 2, delay: 0.5, ease: "bounce.out" });
+    },
+    { scope: container }
+  );
+
   return (
-    <div className="mx-auto flex flex-col justify-center items-center md:mt-24 mt-16 2xl:text-8xl text-[6vw] font-extrabold leading-tight 2xl:leading-tight">
+    <div
+      ref={container}
+      className="mx-auto flex flex-col justify-center items-center md:mt-24 mt-16 2xl:text-8xl text-[6vw] font-extrabold leading-tight 2xl:leading-tight"
+    >
       <div>
         <p className="text-secondary-100 uppercase text-[2vw] md:text-lg tracking-[0.2rem] font-medium text-right md:mr-20 xl:mr-28 mr-9">
           University of St. La Salle
@@ -15,18 +33,24 @@ export function Header() {
         </h1>
       </div>
 
-      <h1>
-        college of{" "}
-        <span className="rounded-full bg-primary-100 md:px-8 px-6 ml-2">
-          engineering
-        </span>
-      </h1>
+      <div className="flex md:space-x-8 space-x-2">
+        <h1>college of </h1>
+        <div className="rounded-full bg-primary-100 md:px-8 px-6 overflow-hidden">
+          <p id="engineering">engineering</p>
+        </div>
+      </div>
 
-      <div className="mt-2 flex ml-12">
+      <div className="md:mt-4 mt-2 flex ml-12">
         <p>& </p>
-        <p className="rounded-full bg-primary-200 md:px-8 px-6 ml-3">technology</p>
+        <div className="rounded-full bg-primary-200 md:px-8 px-6 ml-3 overflow-hidden">
+          <p id="technology">technology</p>
+        </div>
 
-        <Image src={arrow} alt="Arrow" className="xl:h-[80px] lg:h-[70px] md:h-[50px] h-[30px] lg:ml-2 md:-ml-4 -ml-6" />
+        <Image
+          src={arrow}
+          alt="Arrow"
+          className="xl:h-[80px] lg:h-[70px] md:h-[50px] h-[30px] lg:ml-2 md:-ml-4 -ml-6"
+        />
       </div>
     </div>
   );
